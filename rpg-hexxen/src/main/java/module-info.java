8a0d1876e1edba2164a570b9c34b24b9a2dc2dc5
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2021-01-04 09:51:54 +0100

    Added skeletons for the current target systems. That's no endorsement which systems will be supported in the long run. (#8)

--- rpg-hexxen/src/main/java/module-info.java
+++ rpg-hexxen/src/main/java/module-info.java
@@ -2,12 +2,9 @@
  * @author klenkes74
  * @since 1.0.0 2021-01-03
  */
-module de.kaiserpfalzedv.rpg.torg {
+module de.kaiserpfalzedv.rpg.hexxen {
     requires de.kaiserpfalzedv.rpg.core;
     requires org.slf4j;
     requires jakarta.inject.api;
     requires jakarta.enterprise.cdi.api;
-
-    exports de.kaiserpfalzedv.rpg.torg.dice;
-    exports de.kaiserpfalzedv.rpg.torg;
 }
--- rpg-traveller/src/main/java/module-info.java
+++ rpg-traveller/src/main/java/module-info.java
@@ -2,12 +2,11 @@
  * @author klenkes74
  * @since 1.0.0 2021-01-03
  */
-module de.kaiserpfalzedv.rpg.torg {
+module de.kaiserpfalzedv.rpg.traveller {
     requires de.kaiserpfalzedv.rpg.core;
     requires org.slf4j;
     requires jakarta.inject.api;
     requires jakarta.enterprise.cdi.api;
 
-    exports de.kaiserpfalzedv.rpg.torg.dice;
-    exports de.kaiserpfalzedv.rpg.torg;
+    exports de.kaiserpfalzedv.rpg.traveller.spacecrafts;
 }
