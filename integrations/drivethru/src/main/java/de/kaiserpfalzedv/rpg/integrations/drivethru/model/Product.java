649e77c142557d274a2c29301d5971351faaddd7
klenkes74 <rlichti@kaiserpfalz-edv.de>
2021-02-06 12:04:27 +0100

    Renamed the publishers package to model and moved the Service interface.
    Signed-off-by: klenkes74 <rlichti@kaiserpfalz-edv.de>

--- integrations/drivethru/src/main/java/de/kaiserpfalzedv/rpg/integrations/drivethru/model/Product.java
+++ integrations/drivethru/src/main/java/de/kaiserpfalzedv/rpg/integrations/drivethru/model/Product.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package de.kaiserpfalzedv.rpg.integrations.drivethru.publishers;
+package de.kaiserpfalzedv.rpg.integrations.drivethru.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
